29fd7e126ce382aed0086d8fb8dffa11f4c0a804
yingsen2 <250099142@qq.com>
2016-07-11 21:38:27 +0800

    fix bug in admin moidel

--- Application/Home/Controller/AdminCommonController.class.php
+++ Application/Home/Controller/AdminCommonController.class.php
@@ -5,19 +5,17 @@
 
 namespace Home\Controller;
 use Think\Controller;
-class AdminiCommonController extends Controller {
+class AdminCommonController extends Controller {
     
     // 初始化函数
-    public function _initialize();
+    public function _initialize(){
+	    $user_id = check_login_status();
+	    if($user_id == 0){
+	        $this->redirect('index/login');
+	    }
+	}
 
 
 }
 
 
-
-public function AdminiController::_initialize(){
-    $user_id = check_login_status();
-    if($user_id == 0){
-        $this->redirect('admin/login');
-    }
-}
