1ae8b65926f3bf3a52479dde9eff4a4f41cc9fa0
VEN-dsx <dongshixian@gmail.com>
2016-07-11 21:23:45 +0800

    add the framework of admin model

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -1,11 +1,40 @@
 <?php
+
+// 后台用户管理控制器
+
+// 实现与用户账号相关的逻辑
+
 namespace Home\Controller;
 use Think\Controller;
-class XedniController extends AdminController {
-    public function index(){
-        $this->display("index/index");
-    }
+class UserController extends AdminCommonController {
+    // 用户模块初始化函数
+    public function _initialize();
 
-    public function funk(){
-    }
+    // 用户管理首页
+    public function index();
+
+    // 增加一个用户
+    public function adduser();
+
+    // 删除一个用户
+    public function deluser();
+
+    // 更改密码
+    public function chgpsw();
+
+    // 更改名字
+    public function chgname();
+
+
+    // 登陆逻辑
+    public function login();
+
+    // 登出逻辑
+    public function logout();
+}
+
+
+
+public function UserController::_initialize(){
+    parent::_initialize();
 }
